30246017e86cac593d1c4e1235c3d44646d1d75a
Logan Lampton <lampton.logan@gmail.com>
2023-05-20 19:31:30 -0400

    refactored browser router routes; added error page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import Error from "./pages/Error";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>home page</div>}/>
-        <Route path="/testing" element={<div><h2>testing</h2></div>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='*' element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
