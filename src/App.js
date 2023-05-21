3f63a9043c616ea99f1b6b471956e61bcd7ebc8e
Logan Lampton <lampton.logan@gmail.com>
2023-05-21 11:44:46 -0400

    refactored navbar and homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import SharedLayout from "./pages/SharedLayout";
 import About from "./pages/About";
 import Products from "./pages/Products";
 import Error from "./pages/Error";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />}>
+        <Route path='/' element={<SharedLayout />}>
+          <Route index element={<Home/>} />
           <Route path='about' element={<About />} />
           <Route path='products' element={<Products />} />
           <Route path='*' element={<Error />} />
