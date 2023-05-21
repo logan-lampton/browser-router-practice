23e10428e958ae0df721f057d16650f73b688d4f
Logan Lampton <lampton.logan@gmail.com>
2023-05-21 12:17:59 -0400

    rendered products data into products page and created associated single product pages

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,11 +1,22 @@
 import { Link, useParams } from "react-router-dom";
+import products from "../data";
 
 const SingleProduct = () => {
   const { productId } = useParams();
+  const product = products.find((product) => product.id === productId);
+  const {image, name} = product
+
   return (
     <section className='section product'>
-      <h4>Now displaying product {productId}</h4>
-      <Link to='/products' className='btn'>Back to Products page</Link>
+      <h4>
+        {name}
+      </h4>
+      <p>Product ID: {productId}: </p>
+      <img src={image} alt={name}/>
+      <div></div>
+      <Link to='/products' className='btn'>
+        Back to Products page
+      </Link>
     </section>
   );
 };
