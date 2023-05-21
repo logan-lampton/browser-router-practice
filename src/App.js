8b23310c0b09730884c8b31926955dbcce11e695
Logan Lampton <lampton.logan@gmail.com>
2023-05-21 12:06:54 -0400

    set up URL params for listing products

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import About from "./pages/About";
 import Products from "./pages/Products";
 import Error from "./pages/Error";
 import Home from "./pages/Home";
+import SingleProduct from "./pages/SingleProduct";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<SharedLayout />}>
-          <Route index element={<Home/>} />
+          <Route index element={<Home />} />
           <Route path='about' element={<About />} />
           <Route path='products' element={<Products />} />
+          <Route path='products/:productId' element={<SingleProduct />} />
           <Route path='*' element={<Error />} />
         </Route>
       </Routes>
--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,7 +1,11 @@
+import { Link, useParams } from "react-router-dom";
+
 const SingleProduct = () => {
+  const { productId } = useParams();
   return (
     <section className='section product'>
-      <h2>single product</h2>
+      <h4>Now displaying product {productId}</h4>
+      <Link to='/products' className='btn'>Back to Products page</Link>
     </section>
   );
 };
